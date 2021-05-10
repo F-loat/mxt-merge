56de2d1dbeaf40bbc2d7a7699af8bdabdb702dc7
F-loat <chaimaoyuan@foxmail.com>
2021-05-11 02:04:44 +0800

    chore: pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/mxt-merge/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate'
+    })
+  ]
 })
